e4559d6352f69df101924fa48434425dfbadd23d
Gilles Philippart <gilles.philippart@gmail.com>
2018-04-29 21:32:33 +0100

    Rework example

--- examples/project.clj
+++ examples/project.clj
@@ -1,6 +1,11 @@
 (defproject clojure_cukes "1.0.X"
   :description "A demo of Cucumber with Clojure and Leiningen"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :cucumber-feature-paths ["test/features/"]
+
+  :test-paths ["test/acceptance"
+               "test/features"]
+
+  :profiles {:dev {:monkeypatch-clojure-test false
+                    :dependencies [[io.cucumber/cucumber-clojure "2.0.2"]]}}
 
   )
